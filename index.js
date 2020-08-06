8da209398dc1b62304839334da7aa5d0c8949dec
mkrishnan-codes <manu.krishnan@tarams.com>
2020-08-06 15:31:29 +0530

    whitelisted localhost

--- index.js
+++ index.js
@@ -2,19 +2,33 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 const compression = require('compression');
+const cors = require('cors')
+
+var whitelist = ['http://localhost:3000']
+var corsOptions = {
+	origin: function (origin, callback) {
+		if (whitelist.indexOf(origin) !== -1) {
+			callback(null, true)
+		} else {
+			callback(new Error('Not allowed by CORS'))
+		}
+	}
+}
+
+
 const data = require('./data/words.json')
 const smallData = require('./data/smallwords.json');
-app.get('/get-xxl', function (req, res) {
+app.get('/get-xxl', cors(corsOptions), (req, res) => {
 	console.log('Sending xxl data')
 	res.json(data);
-  })
-  app.get('/get-xs', function (req, res) {
+})
+app.get('/get-xs', cors(corsOptions), (req, res) => {
 	console.log('Sending xs data')
 	res.json(smallData);
-  })
-  app.get('*', function (req, res) {
+})
+app.get('*', (req, res) => {
 	res.status(404).end();
-  })
+})
 console.log(`🚀🚀🚀 Starting web server at ${PORT}`);
 app.use(compression());
 app.listen(PORT);
